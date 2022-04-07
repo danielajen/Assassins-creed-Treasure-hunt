e1c984a64ed49f8b4b26cdb97a726d0dc4fc5a52
Daniel Ajenifuja <70176434+danielajen@users.noreply.github.com>
2022-04-06 21:48:54 -0400

    Add project files.

---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        int upgrades = 0;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            // first box
+            pictureBox1.BackgroundImage = Properties.Resources.bebcae4035dd853fea00368f46538ec1;
+            upgrades = upgrades + 10;
+            if(upgrades ==1)
+            {
+                MessageBox.Show("You have found the first weapon! +10 Points, your Weapon is level one, find more to level up!");
+            }
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            // second box
+            pictureBox2.BackgroundImage = Properties.Resources.enemy1;
+        }
+    }
+}
